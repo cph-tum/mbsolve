3b5f47c25c29a08b80789d90a96eb18dad8c6895
Popp, Johannes <johannes.popp@tum.de>
2023-05-25 10:05:00 +0200

    tools: Add autosave and restart option to the SIT setup #52.

--- tools/python/ziolkowski1995.py
+++ tools/python/ziolkowski1995.py
@@ -25,6 +25,8 @@
 import mbsolve.lib as mb
 import mbsolve.solvercpu
 import mbsolve.writerhdf5
+import mbsolve.readerhdf5
+
 
 import math
 import time
@@ -44,19 +46,30 @@ dev.add_region(mb.region("Vacuum left", mat_vac, 0, 7.5e-6))
 dev.add_region(mb.region("Active region", mat_ar, 7.5e-6, 142.5e-6))
 dev.add_region(mb.region("Vacuum right", mat_vac, 142.5e-6, 150e-6))
 
-# initial density matrix
+# initialize for new simulation
 rho_init = mb.qm_operator([ 1, 0 ])
-
-# scenario
 ic_d = mb.ic_density_const(rho_init)
 ic_e = mb.ic_field_const(0.0)
+# scenario
 sce = mb.scenario("Basic", 32768, 200e-15, ic_d, ic_e)
 sce.add_record(mb.record("inv12", 2.5e-15))
 sce.add_record(mb.record("e", 2.5e-15))
-
 # add source
 sce.add_source(mb.sech_pulse("sech", 0.0, mb.source.hard_source, 4.2186e9,
                              2e14, 10, 2e14))
+
+# restart with initialization from autosaved simulation data
+# filename_autosave = "Ziolkowski_Basic_autosave.hdf" # Please choose the right autosave file to restart simulation
+# read = mb.reader.create_instance("hdf5")
+# rho_init = read.read_density(filename_autosave)
+# ic_d = mb.ic_density_autosave(rho_init)
+# ic_e = mb.ic_field_autosave(read.read_field(filename_autosave, "e"))
+# ic_h = mb.ic_field_autosave(read.read_field(filename_autosave, "h"))
+# ic_p = mb.ic_field_autosave(read.read_field(filename_autosave, "p"))
+# # scenario
+# sce = mb.scenario("Basic", 32768, 200e-15, ic_d, ic_e, ic_h, ic_p)
+# sce.add_record(mb.record("inv12", 2.5e-15))
+# sce.add_record(mb.record("e", 2.5e-15))
 
 # run solver
 sol = mb.solver.create_instance("cpu-fdtd-red-2lvl-cvr-rodr", dev, sce)
@@ -71,3 +84,6 @@ wri = mb.writer.create_instance("hdf5")
 outfile = dev.get_name() + "_" + sce.get_name() + "." + wri.get_extension()
 results = sol.get_results()
 wri.write(outfile, sol.get_results(), dev, sce)
+outfile_autosave = dev.get_name() + "_" + sce.get_name() + "_autosave." + wri.get_extension()
+sim_data = sol.get_sim_data()
+wri.autosave(outfile_autosave, sim_data, dev, sce)
